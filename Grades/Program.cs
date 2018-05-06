084f378bb6cd07a82921a0d75e91b0757de8e934
sblasaws <stblasa@gmail.com>
2018-05-05 19:08:55 -0700

    needed to add error handling correctly

--- Grades/Program.cs
+++ Grades/Program.cs
@@ -14,7 +14,7 @@ namespace Grades
 
 
             GradeBook book = new GradeBook();
-            book.Name = null;
+            book.Name  = null;
             book.AddGrade(91);
             book.AddGrade(89.5f);
             book.AddGrade(75);
